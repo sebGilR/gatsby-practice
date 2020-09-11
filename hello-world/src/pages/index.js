e5bcf0b16e8265915c41c5aed7e871e45192c92b
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-09-11 17:13:58 -0500

    Initialize Gatsby project

---
+++ hello-world/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Home  = () => {
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+    </div>
+  )
+}
+
+export default Home;
