7dd0dc13f3978aaf1ca774fbbdde06b275549b78
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-09-11 17:30:20 -0500

    Create basic pages and components

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,10 +1,14 @@
-import React from "react"
+import React from "react";
+import {Link} from "gatsby";
+import Header from "../components/header";
 
 const Home  = () => {
   return (
     <div style={{ color: `purple` }}>
-      <h1>Hello Gatsby!</h1>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="Hello Gatsby!" />
       <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
   )
 }
